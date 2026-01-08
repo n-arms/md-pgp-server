b2daff5ed7725afdfd3b5b8ec7c998ef88e9fc0f
n-arms <75282394+n-arms@users.noreply.github.com>
2026-01-08 13:15:42 -0500

    Add basic server + sqlite db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, extract::Path, routing::get};
+use sqlx::sqlite::SqlitePoolOptions;
+
+#[tokio::main]
+async fn main() {
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("file:data.db")
+        .await
+        .unwrap();
+    // build our application with a single route
+    let app = Router::new().route("/key/{key}", get(handle_get_key));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("localhost:8000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_get_key(Path(key): Path<String>) -> String {
+    format!("Got key {key:?}")
 }
