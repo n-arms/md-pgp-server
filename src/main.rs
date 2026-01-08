5d9eabdfd9ec52e72f5f25d49aaf4e86d148bebb
Jason <jason@jasonx.dev>
2026-01-08 14:05:55 -0500

    add insert user uid function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use axum::{Router, extract::Path, routing::get};
-use sqlx::sqlite::SqlitePoolOptions;
+use axum::{
+    Router,
+    extract::{Path, State},
+    routing::get,
+};
+use sqlx::{SqlitePool, pool, sqlite::SqlitePoolOptions};
 
 #[tokio::main]
 async fn main() {
@@ -9,7 +13,9 @@ async fn main() {
         .await
         .unwrap();
     // build our application with a single route
-    let app = Router::new().route("/key/{key}", get(handle_get_key));
+    let app = Router::new()
+        .route("/key/{key}", get(handle_get_key))
+        .with_state(pool.clone());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("localhost:8000")
@@ -18,6 +24,18 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handle_get_key(Path(key): Path<String>) -> String {
+async fn handle_get_key(State(pool): State<SqlitePool>, Path(key): Path<String>) -> String {
+    insert_user(pool, &key).await;
+
     format!("Got key {key:?}")
+}
+
+async fn insert_user(pool: SqlitePool, fingerprint: &String) {
+    let result = sqlx::query(r#"insert into users (uid) values (?)"#)
+        .bind(&fingerprint)
+        .execute(&pool)
+        .await;
+    if result.is_err() {
+        println!("Error inserting user: {:?}", result.err());
+    }
 }
